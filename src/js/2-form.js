ff6005f94d0b283c9ced3f5398fc59a859cecd35
gramatchi <nik98.champion@gmail.com>
2024-07-17 22:40:25 +0200

    Fix bug-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,23 +3,24 @@ const LOCAL_KEY = 'feedback-form-state';
 
 const formRef = document.querySelector('.feedback-form');
 
+// Инициализация данных формы при загрузке страницы
 function initFormData() {
   const localFormData = JSON.parse(localStorage.getItem(LOCAL_KEY));
-  if (localFormData === null) return;
-
-  const keys = Object.keys(localFormData);
-
-  keys.forEach(key => {
-    formRef.elements[key].value = localFormData[key];
-  });
+  if (localFormData !== null) {
+    formData.email = localFormData.email;
+    formData.message = localFormData.message;
+    formRef.elements.email.value = localFormData.email;
+    formRef.elements.message.value = localFormData.message;
+  }
 }
-
 initFormData();
 
 formRef.addEventListener('input', event => {
   formData[event.target.name] = event.target.value.trim();
   localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
 });
+
+initFormData();
 
 formRef.addEventListener('submit', e => {
   e.preventDefault();
